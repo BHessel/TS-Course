fdc610ffff7695fcc40138ec2ef1579ce55be3eb
Ben Hessel <hesselfitness@gmail.com>
2023-03-03 17:54:51 -0500

    worked through lesson 10

---
+++ src/Avatar.tsx
@@ -0,0 +1,22 @@
+export const FALLBACK_AVATAR_URL = "https://cataas.com/cat/says/hello%20world!";
+export const FALLBACK_AVATAR_ALT_TEXT = "@hello-cat";
+
+type AvatarProps = {
+  url?: string;
+  alt?: string;
+};
+
+function Avatar({
+  url = FALLBACK_AVATAR_URL,
+  alt = FALLBACK_AVATAR_ALT_TEXT,
+}: AvatarProps) {
+  return (
+    <img
+      src={url}
+      alt={alt}
+      onError={() => console.log("TODO - implement me")}
+    />
+  );
+}
+
+export default Avatar;
